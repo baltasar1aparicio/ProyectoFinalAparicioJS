1e52df6651ca4c963260be98fbb59b1ebe0dd6f0
Baltasar Aparicio <baltasar0017@gmail.com>
2023-11-11 15:40:18 -0300

    Proyecto final JAVASCRIPT

---
+++ README.md
@@ -0,0 +1,5 @@
+# PreEntrega3Aparicio
+
+El proyecto es sobre un sitio web de recetas veganas/vegetarianas. En el mismo se incluye un formulario a traves de JavaScript para que los usuarios puedan dar su opinion y asi obtener un feedback. Los mismos datos que
+ingresen los usuarios seran guardados en el storage. Se guarda el value del input nombre aparte para personalizar el titulo, subtitulo y formulario, dando cuenta que no es la primera vez que el usuario ingresa a dejar un comentario en el caso que lo haya hecho (se visualiza al recargar la pagina).
+Ademas se agrega un simpatico boton que se muestra luego del submit, como "regalo" por haber mandado un comentario, el cual permite mostrar ideas de comidas, para cuando uno no sabe que cocinar. Mostrando asi el nombre y pais de origen de una receta "aleatoria" extraida del archivo local JSON.
---
+++ scripts/main.js
@@ -0,0 +1,100 @@
+const titleContact = document.querySelector("#title-contact");
+const nameInput = document.querySelector("#name-input");
+const cityInput = document.querySelector("#city-input");
+const ageInput = document.querySelector("#age-input");
+const emailInput = document.querySelector("#email-input");
+const messageInput = document.querySelector("#message-input");
+const form = document.querySelector("#form");
+let subtitleContact = document.querySelector("#subtitle-contact");
+const nameBox = document.querySelector("#name-box");
+const ageBox = document.querySelector("#age-box");
+const cityBox = document.querySelector("#city-box");
+let recetaRandom = document.getElementById('valor-receta-random')
+let paisRandom = document.getElementById('valor-pais-random')
+let descripcionRandom = document.getElementById('descripcion-receta-random')
+const botonRandom = document.getElementById('button-random')
+let cajaRandom = document.getElementById('caja-random-id')
+const rutaJson = '/scripts/data.json'
+
+ocultarRandom()
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  contactTitleChange(e)
+})
+
+async function mostrarRecetaAleatoria() {
+  try {
+    const response = await fetch(rutaJson);
+    const data = await response.json();
+    const recetas = data.recetas;
+    const recetaAleatoria = recetas[Math.floor(Math.random() * recetas.length)];
+    recetaRandom.textContent = `${recetaAleatoria.nombre}`;
+    descripcionRandom.textContent = `${recetaAleatoria.descripcion}`;
+    paisRandom.textContent = `Pais: ${recetaAleatoria.pais}`;
+  } catch (error) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Algo salió mal!",
+    });
+  }
+}
+
+botonRandom.addEventListener('click', mostrarRecetaAleatoria);
+
+function contactTitleChange() {
+
+  let nameValue = capitalizeFirstLetter(nameInput.value);
+
+  let userData = {
+    name: nameInput.value,
+    city: cityInput.value,
+    age: ageInput.value,
+    email: emailInput.value,
+    message: messageInput.value
+  };
+
+  localStorage.setItem("name-entered", nameValue)
+  localStorage.setItem("user-data", JSON.stringify(userData))
+
+  titleContact.innerText = `Gracias por tu comentario ${nameValue} :)`;
+
+  subtitleContact.remove()
+  form.reset()
+  mostrarRandom()
+};
+
+
+function capitalizeFirstLetter(text) {
+  return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
+}
+
+let nameEntered = localStorage.getItem("name-entered")
+
+function removeRedundantData() {
+  nameBox.remove()
+  cityBox.remove()
+  ageBox.remove()
+}
+function ocultarRandom() {
+  cajaRandom.style.display = "none"
+}
+function mostrarRandom() {
+  cajaRandom.style.display = "flex"
+}
+
+const userData = {
+  name: nameEntered,
+  city: cityInput.value,
+  age: ageInput.value,
+  email: emailInput.value,
+  message: messageInput.value
+}
+
+if (nameEntered !== null) {
+  titleContact.innerText = `Que bueno verte de nuevo por aquí ${nameEntered} :D`
+  subtitleContact.innerText = `¿Quieres dejarnos otro comentario? Adelante!`
+  removeRedundantData()
+  mostrarRandom()
+}
